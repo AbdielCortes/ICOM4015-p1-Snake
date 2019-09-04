0c2f13dce326a277a3d59de677996e507c0344e2
Abdiel Cortes <abdiel.cortes@upr.edu>
2019-09-04 11:55:30 -0400

    Changed grid size

--- src/Worlds/WorldOne.java
+++ src/Worlds/WorldOne.java
@@ -15,8 +15,8 @@ public class WorldOne extends WorldBase{
         super(handler);
 
         //has to be a number bigger than 20 and even
-        GridWidthHeightPixelCount = 40;
-        GridPixelsize = (800/GridWidthHeightPixelCount);
+        GridWidthHeightPixelCount = 30; //size of grid
+        GridPixelsize = (900/GridWidthHeightPixelCount);
         playerLocation = new Boolean[GridWidthHeightPixelCount][GridWidthHeightPixelCount];
         appleLocation = new Boolean[GridWidthHeightPixelCount][GridWidthHeightPixelCount];
 
@@ -28,19 +28,19 @@ public class WorldOne extends WorldBase{
         player.tick();
         if(!appleOnBoard){
             appleOnBoard=true;
-            int appleX = new Random().nextInt(handler.getWorld().GridWidthHeightPixelCount-1);
-            int appley = new Random().nextInt(handler.getWorld().GridWidthHeightPixelCount-1);
+            int appleX = new Random().nextInt(handler.getWorld().GridWidthHeightPixelCount-1); //generates random x coordinates
+            int appleY = new Random().nextInt(handler.getWorld().GridWidthHeightPixelCount-1); //generates random y coordinates
 
-            //change coordinates till one is selected in which the player isnt standing
+            //change coordinates till one is selected in which the player isn't standing
             boolean goodCoordinates=false;
             do{
-                if(!handler.getWorld().playerLocation[appleX][appley]){
+                if(!handler.getWorld().playerLocation[appleX][appleY]){ //checks if player is not in the position of the player
                     goodCoordinates=true;
                 }
             }while(!goodCoordinates);
 
-            apple = new Apple(handler,appleX,appley);
-            appleLocation[appleX][appley]=true;
+            apple = new Apple(handler,appleX,appleY); 
+            appleLocation[appleX][appleY]=true; //sets apple location to random x and random y
 
         }
     }
@@ -48,7 +48,7 @@ public class WorldOne extends WorldBase{
     @Override
     public void render(Graphics g){
         super.render(g);
-        player.render(g,playerLocation);
+        player.render(g,playerLocation); 
     }
 
 }
